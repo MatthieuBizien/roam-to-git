9f600135b8cea7db0eb2ce57b870288e6bc66723
Matthieu Bizien <matthieu@databiz.io>
2020-03-06 23:53:01 +0100

    Use unittests

--- tests.py
+++ tests.py
@@ -1,17 +1,23 @@
+#!/usr/bin/env python3
+import unittest
+
 from rr_download import format_link
 
 
-# Quick and dirty, Unittest in another day
-def test_format_link():
-    tests = [("", ""),
-             ("string", "string"),
-             ("string [[link]].", "string [link](<link.md>)."),
-             ("[[link]] [[other]]", "[link](<link.md>) [other](<other.md>)"),
-             ]
-    for src, target in tests:
-        transformed = format_link(src)
-        assert transformed == target, (src, transformed, target)
+class TestFormatLinks(unittest.TestCase):
+    def test_empty(self):
+        self.assertEquals(format_link(""), "")
+
+    def test_no_link(self):
+        self.assertEquals(format_link("string"), "string")
+
+    def test_one_link(self):
+        self.assertEquals(format_link("string [[link]]."), "string [link](<link.md>).")
+
+    def test_two_links(self):
+        self.assertEquals(format_link("[[link]] [[other]]"),
+                          "[link](<link.md>) [other](<other.md>)")
 
 
 if __name__ == "__main__":
-    test_format_link()
+    unittest.main()
